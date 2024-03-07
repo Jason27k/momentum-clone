c7397255598997d082066daed91a6d4102919d9f
Jason27k <68726620+Jason27k@users.noreply.github.com>
2024-03-06 17:11:46 -0800

    Footer links fixed

--- src/components/FooterLinks.tsx
+++ src/components/FooterLinks.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 
 const FooterLinks = () => {
   return (
-    <div className="flex items-center justify-between text-[#6b6b6b] text-sm w-[60vw]">
+    <div className="flex flex-wrap items-center justify-between text-[#6b6b6b] text-sm w-[60vw]">
       {footerLinks.map((link) => (
-        <Link href="/" key={link}>
+        <Link href="/" key={link} className="mx-auto text-center">
           {link}
         </Link>
       ))}
