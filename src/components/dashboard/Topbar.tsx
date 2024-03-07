541a02961876b76f1a601211d121eb4ce12bf382
Jason27k <68726620+Jason27k@users.noreply.github.com>
2024-03-06 18:36:57 -0800

    Mobile Navbar fix

--- src/components/dashboard/Topbar.tsx
+++ src/components/dashboard/Topbar.tsx
@@ -21,7 +21,7 @@ const Topbar = ({ email, image }: TopbarProps) => {
 
   return (
     <header className="flex h-14 items-center gap-4 border-b bg-gray-100/40 px-6 dark:bg-gray-800/40">
-      <Link className="lg:hidden text-2xl font-bold tracking-tighter" href="#">
+      <Link className="lg:hidden text-2xl font-bold tracking-tighter" href="/">
         Richard
       </Link>
       <div className="flex-1 text-sm font-medium">
@@ -33,13 +33,19 @@ const Topbar = ({ email, image }: TopbarProps) => {
         <span className="text-sm font-medium mr-2">{email}</span>
         <DropdownMenu>
           <DropdownMenuTrigger>
-            <Image
-              alt="Avatar"
-              className="rounded-full"
-              height="32"
-              src={image}
-              width="32"
-            />
+            <div className="flex items-center">
+              <span className="text-sm font-medium mr-2 sm:hidden">
+                {email}
+              </span>
+
+              <Image
+                alt="Avatar"
+                className="rounded-full"
+                height="32"
+                src={image}
+                width="32"
+              />
+            </div>
           </DropdownMenuTrigger>
           <DropdownMenuContent>
             <DropdownMenuItem>
